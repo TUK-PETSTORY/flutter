77e323cb66da4fd884ae0304e61f4c37a1b4bb2a
Hj1218 <wjdguswl2341@naver.com>
2024-07-19 16:09:07 +0900

    [publishing] tapbar 구현

--- lib/src/screens/boast.dart
+++ lib/src/screens/boast.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 
+final List<Tab> tabs = <Tab>[
+  Tab(child: Text('자식자랑', style: TextStyle(fontFamily: 'MainFont'))),
+  Tab(child: Text('육아꿀팁', style: TextStyle(fontFamily: 'MainFont'))),
+  Tab(child: Text('입양', style: TextStyle(fontFamily: 'MainFont'))),
+];
+
+final List<Widget> tabItems = [
+  Center(child: Text('자식자랑', style: TextStyle(fontFamily: 'MainFont'))),
+  Center(child: Text('육아꿀팁', style: TextStyle(fontFamily: 'MainFont'))),
+  Center(child: Text('입양', style: TextStyle(fontFamily: 'MainFont'))),
+];
+
 class Boast extends StatelessWidget {
   const Boast({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('PetStory', style: TextStyle(fontFamily: 'MainFont')),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('PetStory', style: TextStyle(fontFamily: 'MainFont')),
+          bottom: TabBar(
+            tabs: tabs,
+          ),
+        ),
+        body: TabBarView(
+          children: tabItems,
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Boast(),
+  ));
 }
