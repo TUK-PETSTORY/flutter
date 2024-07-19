9a58c37b9c146dd344eaf43806caa4a9b3e1316a
Hj1218 <wjdguswl2341@naver.com>
2024-07-19 15:39:20 +0900

    [publishing] AppBar 추가

---
+++ lib/src/screens/boast.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Boast extends StatelessWidget {
+  const Boast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('PetStory'),
+      ),
+    );
+  }
+}
