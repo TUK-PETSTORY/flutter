199ab7162d714637f11f4718f1ea77641f1a5472
Hj1218 <wjdguswl2341@naver.com>
2024-07-20 21:47:12 +0900

    [publishing] 글 분량과 상관없이 무조건 더보기 생기는 오류 수정

--- lib/src/feed/feed_listitem.dart
+++ lib/src/feed/feed_listitem.dart
@@ -27,6 +27,36 @@ class FeedListItem extends StatefulWidget {
 
 class _FeedListItemState extends State<FeedListItem> {
   bool isExpanded = false;
+  bool showMoreButton = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      //프레임 렌더링 후 실행
+      _measureText();
+    });
+  }
+
+  void _measureText() {
+    //텍스트 높이 측정, 더보기 상태 설정
+    final textPainter = TextPainter(
+      //텍스트 레이아웃 계산
+      text: TextSpan(
+        text: widget.content,
+        style: TextStyle(fontSize: 16, fontFamily: 'MainFont'),
+      ),
+      maxLines: 2,
+      textDirection: TextDirection.ltr, //텍스트 방향(왼쪽 -> 오른쪽)
+    )..layout(maxWidth: MediaQuery.of(context).size.width - 30); //텍스트 레이아웃 계산
+
+    if (textPainter.size.height > 32) {
+      //32px 대략 2줄의 텍스트 높이
+      setState(() {
+        showMoreButton = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +75,7 @@ class _FeedListItemState extends State<FeedListItem> {
             child: Row(
               children: [
                 CircleAvatar(
+                  //유저 프로필
                   radius: 22,
                   backgroundImage: NetworkImage(widget.userProfileUrl),
                 ),
@@ -60,9 +91,10 @@ class _FeedListItemState extends State<FeedListItem> {
                           Text(
                             widget.userName,
                             style: TextStyle(
-                                fontFamily: 'MainFont',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
+                              fontFamily: 'MainFont',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
                           ),
                           Icon(Icons.more_horiz,
                               size: 20, color: Colors.grey), //더보기 아이콘
@@ -71,9 +103,10 @@ class _FeedListItemState extends State<FeedListItem> {
                       Text(
                         widget.subtitle,
                         style: TextStyle(
-                            fontFamily: 'MainFont',
-                            color: Colors.grey[600],
-                            fontSize: 16),
+                          fontFamily: 'MainFont',
+                          color: Colors.grey[600],
+                          fontSize: 16,
+                        ),
                       ),
                     ],
                   ),
@@ -96,12 +129,14 @@ class _FeedListItemState extends State<FeedListItem> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
+                  // 제목
                   widget.title,
                   style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'MainFont',
-                      color: Colors.black87),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'MainFont',
+                    color: Colors.black87,
+                  ),
                 ),
                 SizedBox(height: 4), //요소 사이 간격
                 GestureDetector(
@@ -114,6 +149,7 @@ class _FeedListItemState extends State<FeedListItem> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
+                        //내용
                         widget.content,
                         style: TextStyle(
                           fontSize: 16,
@@ -124,7 +160,7 @@ class _FeedListItemState extends State<FeedListItem> {
                             ? TextOverflow.visible
                             : TextOverflow.ellipsis,
                       ),
-                      if (!isExpanded)
+                      if (showMoreButton && !isExpanded)
                         Text(
                           '더보기',
                           style: TextStyle(
@@ -148,22 +184,23 @@ class _FeedListItemState extends State<FeedListItem> {
                       ),
                     ), //작성날짜
                     Row(
+                      //좋아요 아이콘
                       children: [
                         Icon(Icons.thumb_up_off_alt),
-                        SizedBox(width: 4), //요소사이 간격
+                        SizedBox(width: 4), //요소 사이 간격
                         Text(
                           '${widget.likes}',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ],
-                )
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
