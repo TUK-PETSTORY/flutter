ddeb3e9dd965efe0e90a17447d1174ef66a28127
rvbear <rvbear2000@gmail.com>
2024-07-20 14:01:06 +0900

    [feat] 추천사이트 상단탭 구현 완료

---
+++ lib/src/screens/shop.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+final List<Tab> tabs = <Tab>[
+  Tab(child: Text('전체보기', style: TextStyle(fontFamily: 'MainFont'))),
+  Tab(child: Text('즐겨찾기', style: TextStyle(fontFamily: 'MainFont')))
+];
+
+final List<Widget> tabItems = [
+  Center(child: Text('전체보기', style: TextStyle(fontFamily: 'MainFont'))),
+  Center(child: Text('즐겨찾기', style: TextStyle(fontFamily: 'MainFont')))
+];
+
+class Shop extends StatelessWidget {
+  const Shop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text('Pet Story', style: TextStyle(fontFamily: 'MainFont')),
+          bottom: TabBar(
+            tabs: tabs,
+            indicatorColor: Color(0xffFF4081),
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+            overlayColor: MaterialStatePropertyAll(
+              Color(0xffFFEEEE),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: tabItems,
+        ),
+      ),
+    );
+  }
+}
